73b2b864c4957559f12d83f9f153b087f9f08496
banuaggun <banuaggun@outlook.com>
2023-05-03 17:51:03 +0300

    first commit

---
+++ src/views/EditProject.vue
@@ -0,0 +1,13 @@
+<template lang="">
+  <div>
+    edit project
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="">
+  
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="home">
+        <div v-if="projects.length">
+            <div v-for="project in projects" :key="project/id">
+                <h2>{{ project.title }}</h2>
+                <p>{{ project.details }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "Home",
+    data() {
+        return {
+            projects: [],
+        };
+    },
+    mounted() {
+        fetch("http://localhost:3000/projects")
+            .then((res) => res.json())
+            .then((data) => (this.projects = data))
+            .catch((err) => console.log(err.message));
+    },
+};
+</script>
+<style></style>
